ca7d18e7e63ac48f36ae153286b0160d2dc01564
hunterxcobby <solomonsefah13@gmail.com>
2023-10-09 16:32:29 +0000

    used execve to exedcut

--- shell.c
+++ shell.c
@@ -28,9 +28,42 @@ int main(void)
 
          while(token != NULL)
          {
-            printf("%s\n", token);
-            token = strtok(NULL, delimiters);
+            /* Here we can check if the token is built-in command like (i.e, cd,)
+             * handle it separately or internally
+             */
+
+            /* If it is not built-in, we EXECUTE!!!*/
+            /* This is where to fork*/
+            pid_t pid = fork();
+
+            if (pid == -1)
+            {
+                perror("Fork failed");
+                return (-1);
+            }
+            else if (pid == 0)
+            {
+                /* This is the child process */
+                char *cmdPath = path_check(token); /*Assume we have a function to find the commands path*/
+                if (cmdPath == NULL)
+                {
+                    perror("Command not found");
+                    exit(1); /* Exited with an error*/
+                }
+                
+                /* We execute the program now with execve*/
+                execve(cmdPath, &token, __environ);
+                
+                /* Check if execve fails*/
+                if (execve(cmdPath, &token, __environ) == -1 )
+                {
+                    perror("Failed to execute the program");
+                    exit(EXIT_FAILURE); /* Indicate the command could not be executed*/
+                }
+                
+            }
          }
+
             return(0);
     }
 
